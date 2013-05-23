4c380f6b9493db37eac18776a1f6d361a8df2065
Raynos <raynos2@gmail.com>
2013-05-23 15:19:43 -0700

    initial

--- bin/npmify.js
+++ bin/npmify.js
@@ -18,7 +18,28 @@
         - git commit -m "initial"
         - git push origin master
         - npm publish
+
+    Missing pieces !!!
+
+        - testlingify !!
+        - move some code into index.js so just
+        - ngenplus <name> ./lib/fu.js
+        - write tests for me? Please?
+        - Extract an example from a block comment in the file
+            and place that in the README!
 */
 
 var argv = require("optimist")
 var shell = require("shelljs")
+var fs = require("fs")
+var path = require("path")
+
+if (argv.help) {
+    fs.readFileSync(path.join(__dirname, "usage.txt")).pipe(process.stdout)
+} else {
+    main()
+}
+
+function main() {
+
+}
